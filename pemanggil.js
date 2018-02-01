6c1458e9f1ca06af2764ee8429035a6c4862811e
achmadandik <35857913+achmadandik@users.noreply.github.com>
2018-02-01 17:41:31 +0700

    Add files via upload

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="/2018/01/2.html"}
 if(x=="3"){xxx="/2018/01/3.html"}
 if(x=="4"){xxx="/2018/01/4.html"}
 if(x=="5"){xxx="/2018/01/5.html"}
-if(x=="6"){xxx="/2018/01/6.html"}a_to_ve[i].href="https://achsafelink.cf"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="/2018/01/6.html"}a_to_ve[i].href="https://www.achsafelink.cf"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
